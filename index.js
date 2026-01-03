bd9598e5ea1a9c837580f8226f2e2177bec475a0
julianalabellas <julianalabellas@gmail.com>
2026-01-03 20:17:14 +0100

    Update index.js
    tentando corrigir erro de link com readymag

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const PORT = process.env.PORT || 3000;
 // ============================================================
 app.use(cors());
 app.use(express.json());
-app.use(express.static('.'));
 
 // ============================================================
 // INICIALIZAÇÃO
